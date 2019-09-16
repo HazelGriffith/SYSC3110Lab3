416c3bb07db63099d2e6ec27eebd60fc7e139271
hazelgriffith <hazelgriffith@cb5109-22.labs.sce.carleton.ca>
2019-09-16 12:09:19 -0400

    prints "Address book"

---
+++ src/Addressbook.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+
+public class Addressbook {
+	
+	private ArrayList<BuddyInfo> buddiesCollection = new ArrayList();
+	
+	/**
+	 * 
+	 */
+	public Addressbook() {
+		
+	}
+
+	/**
+	 * @return the buddies
+	 */
+	private ArrayList<BuddyInfo> getBuddies() {
+		return buddiesCollection;
+	}
+
+	/**
+	 * @param buddies the buddies to set
+	 */
+	private void setBuddies(ArrayList<BuddyInfo> buddies) {
+		buddiesCollection = buddies;
+	}
+
+	/**
+	 * @param BuddyInfo object
+	 */
+	private void addBuddy(BuddyInfo buddy) {
+		buddiesCollection.add(buddy);
+	}
+	
+	/**
+	 * @param BuddyInfo object
+	 */
+	private void removeBuddy(BuddyInfo buddy) {
+		for (BuddyInfo bud: buddiesCollection) {
+			if (bud.equals(buddy)){
+				buddiesCollection.remove(buddy);
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param int index
+	 */
+	private void removeBuddy(int index) {
+		buddiesCollection.remove(index);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Address book");
+	}
+	
+}
